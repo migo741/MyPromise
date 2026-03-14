8c24b90c92e7daf4dc654d7216eb6f0f2fc3a629
yaho <1362438922@qq.com>
2026-03-14 10:58:46 +0800

    js版一些bug修改

--- Mypromise.js
+++ Mypromise.js
@@ -10,35 +10,44 @@ class MyPromise {
       if (this.#state !== "pending") return;
       this.#state = "fulfilled";
       this.#data = val;
+      this.#onFulfilled.forEach((fn) => fn());
     };
     const reject = (val) => {
       if (this.#state !== "pending") return;
       this.#state = "rejected";
       this.#error = val;
+      this.#onReject.forEach((fn) => fn());
     };
     excutor(resolve, reject);
   }
   then(onFulfilled, onReject) {
-    if (this.#state === "pending") {
-      this.#onFulfilled.push(() => onFulfilled(this.#data));
-      this.#onReject.push(() => onReject(this.#error));
-    }
-    if (this.#state === "fulfilled") {
-      this.#onFulfilled.forEach((fn) => fn());
-    }
-    if (this.#state === "rejected") {
-      this.#onReject.forEach((fn) => fn());
-    }
-    return new MyPromise((resolve, reject) => {});
+    return new MyPromise((resolve, reject) => {
+      const fulfilledFn = () => {
+        try {
+          const result = onFulfilled ? onFulfilled(this.#data) : this.#data;
+          resolve(result);
+        } catch (err) {
+          reject(err);
+        }
+      };
+      const rejectedFn = () => {
+        try {
+          const result = onReject ? onReject(this.#error) : this.#data;
+          resolve(result);
+        } catch (err) {
+          reject(err);
+        }
+      };
+      if (this.#state === "pending") {
+        this.#onFulfilled.push(() => fulfilledFn(this.#data));
+        this.#onReject.push(() => rejectedFn(this.#error));
+      }
+      if (this.#state === "fulfilled") {
+        fulfilledFn();
+      }
+      if (this.#state === "rejected") {
+        rejectedFn();
+      }
+    });
   }
 }
-
-const p = new Promise((resolve, reject) => {
-  resolve(1);
-})
-  .then((val) => {
-    console.log(val);
-  })
-  .then((val) => {
-    console.log(val + 1);
-  });
